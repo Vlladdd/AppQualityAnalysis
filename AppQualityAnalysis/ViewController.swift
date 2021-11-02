4c6527eb167bdb32af3b340e3d271744b345c16c
Vlad Nechyporenko <vlladdd28@gmail.com>
2021-11-02 03:47:02 +0200

    Initial Commit

---
+++ AppQualityAnalysis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppQualityAnalysis
+//
+//  Created by Vlad Nechiporenko on 02.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
